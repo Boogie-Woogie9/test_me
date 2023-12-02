f20fe76276295fbed22613efaf2a3aa16d268a7a
Евсеенков Иван Александрович <evseenkov.2003@mail.ru>
2023-12-02 13:44:53 +0300

    version with migrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+
+  resources :users
   get 'signup' => 'users#new'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
