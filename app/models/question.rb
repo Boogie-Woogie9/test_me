2b9ddd6509523eb350bfa6de879154c15cbfe3bb
Евсеенков Иван Александрович <evseenkov.2003@mail.ru>
2023-12-22 00:54:33 +0300

    create-edit-tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,9 @@
 class Question < ApplicationRecord
   belongs_to :quiz
   validates :correct_answer, inclusion: { in: 1..4 }
-
+  validates :answer1, :answer2, :answer3, :answer4, 
+    presence: true,
+    length: { maximum: 50 }
+  validates :correct_answer, presence: true 
   # has_one_attached :image
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,10 @@
 class Quiz < ApplicationRecord
   has_many :questions,  dependent: :destroy, inverse_of: :quiz
   accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
-
+  validates :title, 
+    presence: true, 
+    length: { maximum: 50 },
+    uniqueness: { case_sensitive: false} 
   # def correct_answer_text
   #   # answer1, answer2, answer3, answer4 are in the model
   #   send("answer#{correct_answer}")
